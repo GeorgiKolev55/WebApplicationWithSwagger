143bc00e9a348dd0414aa58c14cc29fc7bd4d923
GeorgiKolev55 <gkolev55@abv.bg>
2021-08-18 07:50:03 +0300

    StartUp class update

--- WebApplicationWithSwagger/Startup.cs
+++ WebApplicationWithSwagger/Startup.cs
@@ -30,11 +30,15 @@ namespace WebApplicationWithSwagger
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+
+             app.UseSwagger();
 
             if (env.IsDevelopment())
             {
-               
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+                });
 
                 app.UseDeveloperExceptionPage();
             }
@@ -44,12 +48,7 @@ namespace WebApplicationWithSwagger
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
-            });
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
