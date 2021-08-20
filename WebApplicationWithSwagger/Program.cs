07fc1072ab2addc53c4624f26abb090adfb3b944
GeorgiKolev55 <gkolev55@abv.bg>
2021-08-20 19:54:44 +0300

    db

--- WebApplicationWithSwagger/Program.cs
+++ WebApplicationWithSwagger/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 namespace WebApplicationWithSwagger
 {
@@ -24,3 +25,4 @@ namespace WebApplicationWithSwagger
                 });
     }
 }
+
--- WebApplicationWithSwagger/Startup.cs
+++ WebApplicationWithSwagger/Startup.cs
@@ -33,8 +33,9 @@ namespace WebApplicationWithSwagger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-        
+
            
+            var cv = Environment.GetEnvironmentVariable("VaultUri");
             services.AddControllersWithViews();
             services.AddSwaggerGen();
             services.AddScoped<IBookService, BookService>();
